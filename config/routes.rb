5567c6bfb7cdc5cc95f75008f8c8153abf5bdfe5
Beianca <beiancayousif89@gmail.com>
2017-05-15 21:39:18 +0000

    Create Document with carrierwave gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  
+ 
   devise_for :users
   get 'home/index'
+  
+  resources :documents, only: [:index, :new, :create, :destroy]
+   root "documents#index"
 
-  root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
